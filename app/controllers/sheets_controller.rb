f3d2ef5848c10a9926ccbdc441ba4d79c497ae53
kawamura-chuko <kawamura123chuko910@gmail.com>
2023-04-29 07:49:57 +0900

    Add: show,index,updateの雛形

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,7 +1,28 @@
 class SheetsController < ApplicationController
+  before_action :set_sheet, only: %i[ show update destroy ]
+
   def index
+    @sheets = Sheet.all
   end
 
   def show
+  end
+
+  def update
+    if @sheet.update(sheet_params)
+      redirect_to @sheet, notice: "Sheet was successfully updated."
+    else
+      redirect_to @sheet, alert: "入力してください"
+    end
+  end
+
+  private
+
+  def set_sheet
+    @sheet = Sheet.find(params[:id])
+  end
+
+  def sheet_params
+    params.require(:sheet).permit(:title, :level, :comma_joined_mml)
   end
 end
