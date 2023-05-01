e323b37cf519b521c125e1dc3b9f1c7e70898f8c
kawamura-chuko <kawamura123chuko910@gmail.com>
2023-05-02 08:12:48 +0900

    Add: 譜面のcreate処理

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -3,9 +3,27 @@ class SheetsController < ApplicationController
 
   def index
     @sheets = Sheet.all
+    @level = Sheet.types_of_selectable_levels
+  end
+
+  def new
+    @sheet = Sheet.new(level_params)
+    # 自動作曲はまだ未実装のため、仮データを設定
+    @sheet[:title] = '無題'
+    @sheet[:comma_joined_mml] = 'O5L4d,O4L4a,O4L8g,O4L8g'
   end
 
   def show
+  end
+
+  def create
+    @sheet = Sheet.new(sheet_params)
+    if @sheet.save
+      redirect_to @sheet, success: t('.success')
+    else
+      flash.now['danger'] = @sheet.errors.full_messages.join(", ")
+      render :new
+    end
   end
 
   def update
@@ -22,6 +40,10 @@ class SheetsController < ApplicationController
   end
 
   private
+
+  def level_params
+    params.permit(:level)
+  end
 
   def set_sheet
     @sheet = Sheet.find(params[:id])
