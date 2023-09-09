95e189e2f19b24c703ad014a6076770182c5529a
kawamura-chuko <kawamura123chuko910@gmail.com>
2023-09-09 13:04:42 +0900

    Add: システムスペック(UserSessions)

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,6 +1,7 @@
 module LoginMacros
   def login_as(user)
     visit root_path
+    find('.navbar-toggler-icon').click
     click_link 'ログイン'
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: 'password'
